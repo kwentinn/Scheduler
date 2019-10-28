f6a68c4995f23ba490e1966e54bf4c5393549c05
kwentinn <kwentinn@gmail.com>
2019-10-28 22:42:34 +0100

    méthode d'archivage + ctor vide

--- scheduler/Calendar.cs
+++ scheduler/Calendar.cs
@@ -13,10 +13,22 @@ namespace Scheduler.Domain
 		public List<Appointment> Appointments { get; private set; }
 		public List<Organiser> Organisers { get; private set; }
 
-		public Calendar(Guid id, string name):base(id)
+		public Calendar() { }
+		public Calendar(Guid id, string name) : base(id)
 		{
-			if (string.IsNullOrEmpty(name)) throw new ApplicationException("Calendar name is required");
+			if (string.IsNullOrEmpty(name))
+				throw new ApplicationException("Calendar name is required");
+
 			Name = name;
 		}
+
+		public void Archive()
+		{
+			if (IsArchived)
+				throw new ApplicationException("Calendar already archived");
+
+			IsArchived = true;
+		}
+
 	}
 }
