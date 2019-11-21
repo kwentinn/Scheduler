ff0663a87ddc56d0ee85e80eae3df59a3b898313
kwentinn <kwentinn@gmail.com>
2019-11-21 01:04:49 +0100

    add a static class to define the AddPolicyValidationProvider method
    to be used in Startup.cs

---
+++ Scheduler.Domain/Policies/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Kledex.Extensions;
+using Kledex.Validation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Scheduler.Domain.Policies
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IKledexServiceBuilder AddPolicyValidationProvider(this IKledexServiceBuilder builder)
+		{
+			builder.Services.AddScoped<IValidationProvider, PolicyValidationProvider>();
+
+			return builder;
+		}
+	}
+}
