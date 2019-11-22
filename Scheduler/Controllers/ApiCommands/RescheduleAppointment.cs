7d6d0b73a5df73ad58add56aa7699428bbd64941
Quentin Villevieille <quentin.villevieille@studi.fr>
2019-11-22 17:34:03 +0100

    command updated

--- Scheduler/Controllers/ApiCommands/RescheduleAppointment.cs
+++ Scheduler/Controllers/ApiCommands/RescheduleAppointment.cs
@@ -5,6 +5,7 @@ namespace Scheduler.Controllers.ApiCommands
 	public class RescheduleAppointment
 	{
 		public Guid AppointmentId { get; set; }
+		public Guid CalendarId { get; set; }
 		public DateTime NewUtcStart { get; set; }
 		public DateTime NewUtcEnd { get; set; }
 	}
