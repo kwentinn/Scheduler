7f0dc6801f78a2e56e065546aa1c56f787e2ebea
Quentin Villevieille <quentin.villevieille@studi.fr>
2019-11-22 17:32:25 +0100

    IPolicy cleanup

--- Scheduler.Domain/Policies/IPolicy.cs
+++ Scheduler.Domain/Policies/IPolicy.cs
@@ -3,13 +3,6 @@ using System.Threading.Tasks;
 
 namespace Scheduler.Domain.Policies
 {
-	public interface IPolicy<IDomainCommand, IAggregateRoot>
-		where IDomainCommand : Kledex.Domain.IDomainCommand
-		where IAggregateRoot : Kledex.Domain.IAggregateRoot
-	{
-		Task<PolicyResult> CanExecuteAsync(IDomainCommand command, IAggregateRoot aggregateRoot);
-	}
-
 	public interface IPolicy<ICommand>
 		where ICommand : Kledex.Commands.ICommand
 	{
